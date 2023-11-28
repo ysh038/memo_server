3fce5f05172f3b17a6cca7f8f14c0ce42936e609
ysh038 <ysh038@naver.com>
2023-11-29 01:06:59 +0900

    /auth url 수정

--- memo.py
+++ memo.py
@@ -91,14 +91,10 @@ def onLogin():
     url = f'https://nid.naver.com/oauth2.0/authorize?{urlencoded}'
     return redirect(url)
 
-@app.route("/auth")
-def test():
-    return "hello auth"
-
 # 아래는 Redirect URI 로 등록된 경우 호출된다.
 # 만일 본인의 Redirect URI 가 http://localhost:8000/auth 의 경우처럼 /auth 대신 다른 것을
 # 사용한다면 아래 @app.route('/auth') 의 내용을 그 URL 로 바꿀 것
-@app.route('/memo/auth')
+@app.route('/auth')
 def onOAuthAuthorizationCodeRedirected():
     # TODO: 아래 1 ~ 4 를 채워 넣으시오.
     # 1. redirect uri 를 호출한 request 로부터 authorization code 와 state 정보를 얻어낸다.
